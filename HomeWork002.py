2e26d69afff5b8f44d78268fe8ce06f532e4ebb1
Vadim <vsuffering@gmail.com>
2022-12-12 17:08:04 +1000

    Create HW002

---
+++ HomeWork002.py
@@ -0,0 +1,34 @@
+# Задача № 7. Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
+# (расшифровка этого выражения not (x[0] or x[1] or x[2] = not x[0] and not x[1] and not x[2]) для всех значений предикат.
+
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
+
+
+#not(x or y or z) == not x or not y or not z  
+# xyz = ['X', 'Y', 'Z']
+# print("x y z")
+# for x in range(0, 2):
+# 	for y in range(0, 2):
+# 		for z in range(0, 2):
+# 			result = x == ((y and z) <= x)
+# 			print(x, y, z, result)
