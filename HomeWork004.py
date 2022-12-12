286221b6e528e97fb8f8052f61d75c3abfeaab84
Vadim <vsuffering@gmail.com>
2022-12-12 17:51:38 +1000

    Create HW004

---
+++ HomeWork004.py
@@ -0,0 +1,15 @@
+# Задача № 9. Напишите программу, которая по заданному номеру четверти,
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+
+NumberQuarter = int(input('Введите номер четверти: '))
+
+if NumberQuarter == 1:
+    print(f'1 -> x > 0; y > 0')
+elif NumberQuarter == 2:
+    print(f'2 -> x < 0; y > 0')
+elif NumberQuarter == 3:
+    print(f'3 -> x < 0; y < 0')
+elif NumberQuarter == 4:
+    print(f'4 -> x > 0; y < 0')
+else:
+    print(f'Такой четверти нет!')
