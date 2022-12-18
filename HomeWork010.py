9c44f03d5f81f461e065b49b72ec56d5ecb992ce
Vadim <vsuffering@gmail.com>
2022-12-18 11:59:24 +1000

    Change HW010

--- HomeWork010.py
+++ HomeWork010.py
@@ -1,1 +1,7 @@
 # Задача №5: Реализуйте алгоритм перемешивания списка.
+
+import random
+lst =[random.randint(0,10) for i in range(random.randint(5,20))]
+print(f"Список:\n{lst}")
+random.shuffle(lst)
+print(f"Перемешанный список:\n{lst}")
