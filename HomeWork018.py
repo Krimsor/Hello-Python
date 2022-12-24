b0090ef0c6d23ca1aee883f0162a4ce5b1a47209
Krimsor <v_suffering@mail.ru>
2022-12-25 01:34:29 +1000

    Change  README.md 4

--- HomeWork018.py
+++ HomeWork018.py
@@ -3,6 +3,6 @@
 
 lst = list(map(int, input("Введите числа через пробел:\n").split()))
 print(f"Исходный список: {lst}")
-new_lst = []
-[new_lst.append(i) for i in lst if i not in new_lst]
-print(f"Список из неповторяющихся элементов: {new_lst}")
+lst1 = []
+[lst1.append(i) for i in lst if i not in lst1]
+print(f"Список из неповторяющихся элементов: {lst1}")
