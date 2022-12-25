bd3c8c11852a1990bc8405d75099fef86d129e86
Krimsor <v_suffering@mail.ru>
2022-12-25 19:49:27 +1000

    Change  HW018 1

--- HomeWork018.py
+++ HomeWork018.py
@@ -1,10 +1,19 @@
 # Здача №18 Задайте последовательность чисел.
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
+# lst = list(map(int, input("Введите числа через пробел:\n").split()))
+# print(f"Исходный список: {lst}")
+# lst1 = []
+# [lst1.append(i) for i in lst if i not in lst1]
+# print(f'Список из неповторяющихся элементов: {lst1}')
+
+
+from collections import Counter
+
 lst = list(map(int, input("Введите числа через пробел:\n").split()))
 print(f"Исходный список: {lst}")
-lst1 = []
-[lst1.append(i) for i in lst if i not in lst1]
+
+counter = Counter(lst)
+
+lst1 = [elem for elem, n in counter.items() if n == 1]
 print(f'Список из неповторяющихся элементов: {lst1}')
-
-
