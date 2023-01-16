4dba6a481d2a2c3dcd73e9782dc9a4170a12aa1a
Krimsor <v_suffering@mail.ru>
2023-01-16 16:28:53 +1000

    change HW025

--- HomeWork024.py
+++ HomeWork024.py
@@ -1,9 +1,5 @@
 # Здача №24 Реализуйте RLE алгоритм:
 # реализуйте модуль сжатия и восстановления данных.
 
-city = ['Moscow', 'Khabarovsk', 'Vladivostok']
-
-data = list(enumerate(city,1))
-print(data)
 
 
